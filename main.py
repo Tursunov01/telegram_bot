bc6c5dfffa60759c65777ca21ab293fe280700a8
Jasur <mr.jasur09@mail.ru>
2019-07-19 13:44:40 +0500

    problem with heroku

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import telebot
+import os
+from flask import Flask, request
+import logging
+
+
+
+
 #names = []
 
 bot = telebot.TeleBot('883652979:AAFZDW2E8mcJ8dbTq9Lcct-YTZ4uxcnJoBw')
@@ -305,4 +312,23 @@ def send_text(message):
             names.clear()'''
 
 
-bot.polling(none_stop=True, interval=0)
+if "HEROKU" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://dashboard.heroku.com/apps/izabella-mebel-bot") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
+else:
+    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
+    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+    bot.remove_webhook()
+    bot.polling(none_stop=True)
