dc4f33ae493d53cbd1d3e31d3f33569cd27fc534
Jasur <mr.jasur09@mail.ru>
2019-07-20 16:34:53 +0500

    work on server

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from telebot import types
 
 bot = telebot.TeleBot('883652979:AAFZDW2E8mcJ8dbTq9Lcct-YTZ4uxcnJoBw')
 
+server = flask.Flask(__name__)
+
 keyboard1 = telebot.types.ReplyKeyboardMarkup(True, True)
 keyboard1.row('Наша продукция').row('Контакты').row('Наш адрес')
 
@@ -330,22 +332,22 @@ else:
     bot.remove_webhook()
     bot.polling(none_stop=True)'''
 
-server = flask.Flask(__name__)
+
 
 
 @server.route('/' + '883652979:AAFZDW2E8mcJ8dbTq9Lcct-YTZ4uxcnJoBw', methods=['POST'])
-def get_message():
-    bot.process_new_updates([types.Update.de_json(
-        flask.request.stream.read().decode("utf-8"))])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(flask.request.stream.read().decode("utf-8"))])
     return "!", 200
 
 
-@server.route('/', methods=["GET"])
-def index():
+@server.route("/")
+def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://izabella-mebel-bot.herokuapp.com/{}".format('izabella-mebel-bot', '883652979:AAFZDW2E8mcJ8dbTq9Lcct-YTZ4uxcnJoBw'))
-    return "Hello from Heroku!", 200
+    bot.set_webhook(url='https://izabella-mebel-bot.herokuapp.com/' + '883652979:AAFZDW2E8mcJ8dbTq9Lcct-YTZ4uxcnJoBw')
+    return "!", 200
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    server.debug = True
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
